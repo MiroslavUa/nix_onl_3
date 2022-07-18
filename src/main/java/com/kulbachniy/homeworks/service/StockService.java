7c407054e7bdad73f83cb4debc399b0ff913cd2c
Miroslav <mirik1983@gmail.com>
2022-07-18 11:12:34 -0400

    hw13

--- src/main/java/com/kulbachniy/homeworks/service/StockService.java
+++ src/main/java/com/kulbachniy/homeworks/service/StockService.java
@@ -8,35 +8,7 @@ import org.slf4j.LoggerFactory;
 public class StockService extends DerivativeService<Stock> {
     private static final Logger LOGGER = LoggerFactory.getLogger(StockService.class);
 
-//    private final StockRepository stockRepository;
-
     public StockService(CrudRepository<Stock> repository){
         super(repository);
     }
-
-//    public void save(Stock stock) {
-//        stockRepository.save(stock);
-//    }
-
-//    public void saveAll(List<Stock> stocks) {stockRepository.saveAll(stocks);}
-
-//    public void update(Stock stock){
-//        stockRepository.update(stock);
-//    }
-
-//    public void delete(String id){
-//        stockRepository.delete(id);
-//    }
-
-//    public Stock findByTicker(String ticker){
-//        return stockRepository.findByTicker(ticker);
-//    }
-
-//    public Stock findById(String id){
-//        return stockRepository.findById(id);
-//    }
-
-//    public List<Stock> getAll(){
-//        return stockRepository.getAll();
-//    }
 }
