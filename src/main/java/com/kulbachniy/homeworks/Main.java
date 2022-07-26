8b850da8439ae5360e7eb0707ed7ea833ad3057e
Miroslav <mirik1983@gmail.com>
2022-07-26 05:16:05 -0400

    hw15

--- src/main/java/com/kulbachniy/homeworks/Main.java
+++ src/main/java/com/kulbachniy/homeworks/Main.java
@@ -1,17 +1,6 @@
 package com.kulbachniy.homeworks;
 
-import com.kulbachniy.homeworks.model.containers.DerivativeComparator;
-import com.kulbachniy.homeworks.model.containers.lists.AbstractDerivativeList;
-import com.kulbachniy.homeworks.model.containers.lists.LinkedDerivativeList;
-import com.kulbachniy.homeworks.model.containers.trees.BinaryTree;
-import com.kulbachniy.homeworks.model.derivative.*;
-import com.kulbachniy.homeworks.model.containers.ContainerDerivative;
-
 import java.io.IOException;
-import java.sql.SQLOutput;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
